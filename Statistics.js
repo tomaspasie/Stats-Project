a775b797b11183fe49dc800d58393e82b360152f
Tomas Pasiecznik <42984839+tomaspasie@users.noreply.github.com>
2020-03-12 00:23:36 -0400

    Added Quartiles and Skewness

--- Statistics.js
+++ Statistics.js
@@ -9,10 +9,19 @@ class Statistics extends calculator{
         return mean;
 	}
 	Median(values) {
+		values.sort();
 		let numValues = values.length;
-		let position = ((numValues+1)/2)
-		let median = values[position-1]
-        return median;
+		
+		if (numValues%2==0){
+			let position1 = this.quotient(numValues,2);
+			let position2 = this.sum(this.quotient(numValues,2),1)
+			let median = this.quotient(values[position1]+values[position2],2);
+			return median;
+		} else{
+			let position = ((numValues+1)/2)
+			let median = values[position-1]
+			return median;
+		}
 	}
 
 	Mode(values){
@@ -36,13 +45,30 @@ class Statistics extends calculator{
 		return std;
 	}
 
+	
+	Quartiles(values) {
+		let num = values.length;
+		let Q1 = this.product(.25,num+1);
+		let Q3 = this.product(.75,num+1);
+		let Q2 = Q3 - Q1;
+		let quartiles = 'Lower Quartile (Q1): ' + Q1 + ' | Middle Quartile (Q2): '+ Q2 + ' | Upper Quartile (Q3): '+ Q3;
+		return quartiles
+	}
+
+	
+	Skewness(values) {
+		let mean = this.Mean(values);
+		let std = this.standardDev(values);
+		let skew = 0;
+		var value;
+		for (value in values){
+			skew += this.power(this.quotient(values[value] - mean, std), 3);
+		}
+		let skewness = this.quotient(skew,values.length);
+		return skewness;
+	}
+	
 	/*
-	Quartiles() {
-		
-	}
-	Skewness() {
-		
-	}
 	sampleCorrelation() {
 		
 	}
