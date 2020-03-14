e9c73e2b7bc9cddfddad06ed57bf72df19780826
Tomas Pasiecznik <42984839+tomaspasie@users.noreply.github.com>
2020-03-13 21:38:21 -0400

    Added Simple Random Sampling, Simple Systematic Sampling, & Confidence Interval

--- Sampling.js
+++ Sampling.js
@@ -1,15 +1,51 @@
-const Calculator = require('./Calculator');
-class Sampling extends Calculator {
+const statistics = require('./statistics');
+
+class Sampling extends statistics {
+	
+	simpleRandomSampling(values,sampleSize) {
+		let sample = [];
+		let length = values.length;
+		for (var i = 0; i < sampleSize; i++){
+			let random = Math.round(Math.random()*length);
+			if (random != 10){
+				sample += values[random]+' ';
+			} else{
+				i-=1;
+			}
+		}
+		
+		return sample;
+	}
+	
+	systematicSampling(values,every){
+		let sample = [];
+		let everyNum;
+		let length = values.length;
+		for (var i = 1; i < length+1; i++){
+			everyNum = every*i;
+			if(everyNum>length){
+				break;
+			} else{
+				sample += values[everyNum-1]+' ';
+			}
+		}
+
+		return sample;
+	}
+
+	confidenceInterval(values) {
+		let length = values.length;
+		let mean = this.Mean(values);
+		let std = this.standardDev(values);
+		let z = 1.960
+		let lower = ((mean) - z * ((std)/(Math.sqrt(length))));
+		let upper = ((mean) + z * ((std)/(Math.sqrt(length))));
+		let confidence = lower + " ± " + upper;
+		
+		return confidence;
+	}
+
 	/*
-	simpleRandomSampling() {
-		
-	}
-	systematicSampling(){
-		
-	}
-	confidenceInterval() {
-		
-	}
 	marginOfError() {
 		
 	}
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Index
 const statistics = require('./statistics');
+const sampling = require('./sampling');
 let stats = new statistics;
+let sample = new sampling;
 
+console.log('----------------------------------------------------------------------------------------------');
+console.log('Statistics Functions');
+console.log('----------------------------------------------------------------------------------------------');
 console.log('The Mean is ' + stats.Mean([10,20,30,40]));
 console.log('The Median is ' + stats.Median([1,2,3,4,5,6]));
 console.log('The Mode is ' + stats.Mode([1,2,3,3,3,4,4,4,5]));
@@ -13,3 +18,9 @@ console.log('The Sample Correlation is ' + stats.sampleCorrelation([15,37,59,78,
 console.log('The Population Correlation is ' + stats.populationCorrelation([15,37,59,78,91],[6,21,48,67,88]));
 console.log('The Z-Score is ' + stats.zscore([15,37,59,78,91,6,21,48,67,88], 78));
 console.log('The Mean Deviation is ' + stats.meanDeviation([15,37,59,78,91,6,21,48,67,88]));
+console.log('----------------------------------------------------------------------------------------------');
+console.log('Population Sampling Functions');
+console.log('----------------------------------------------------------------------------------------------');
+console.log('Simple Random Sampling: ' + sample.simpleRandomSampling([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5));
+console.log('Simple Systematic Sampling: ' + sample.systematicSampling([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2));
+console.log('Confidence Interval: ' + sample.confidenceInterval([15,37,59,78,91,6,21,48,67,88]));
