3ee29e15e1c75b727d40750e86b9d4d163f0b292
Tomas Pasiecznik <42984839+tomaspasie@users.noreply.github.com>
2020-03-14 20:53:07 -0400

    Added Templates For Tests

--- tests/statistics.test.js
+++ tests/statistics.test.js
@@ -1,6 +1,89 @@
 const calculator = require('../calculator');
 let calc = new calculator;
 
-//test('The calculator returns the mean of the values.', () => {
-//    expect(calc.Mean()).toBe();
-//});
+// Statistics Functions
+
+test('Mean', () => {
+    expect(calc.Mean([10,20,30,40])).toBe(25); // Change Values to Numbers From Random Generator
+});
+
+/*
+
+test('Median', () => {
+    expect().toBe();
+});
+
+test('Mode', () => {
+    expect().toBe();
+});
+
+test('Variance', () => {
+    expect().toBe();
+});
+
+test('Standard Deviation', () => {
+    expect().toBe();
+});
+
+test('Quartiles', () => {
+    expect().toBe();
+});
+
+test('Skewness', () => {
+    expect().toBe();
+});
+
+test('Sample Correlation', () => {
+    expect().toBe();
+});
+
+test('Population Correlation', () => {
+    expect().toBe();
+});
+
+test('Z-Score', () => {
+    expect().toBe();
+});
+
+test('Mean Deviation', () => {
+    expect().toBe();
+});
+
+*/
+
+// Population Sampling Functions
+
+/*
+test('Simple Random Sampling', () => {
+    expect().toBe();
+});
+
+test('Simple Systematic Sampling', () => {
+    expect().toBe();
+});
+
+test('Sample Confidence Interval', () => {
+    expect().toBe();
+});
+
+test('Margin of Error', () => {
+    expect().toBe();
+});
+
+test('Population Confidence Interval', () => {
+    expect().toBe();
+});
+
+test('Cochran’s Sample Size', () => {
+    expect().toBe();
+});
+
+test('Sample Size (Unknown Population Standard Deviation)', () => {
+    expect().toBe();
+});
+
+test('Sample Size (Known Population Standard Deviation)', () => {
+    expect().toBe();
+});
+
+*/
