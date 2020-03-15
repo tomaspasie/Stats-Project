c30a06f028d9e4030ab11c5a7a1ae227b2be0cf5
mb622 <48958432+mb622@users.noreply.github.com>
2020-03-15 02:33:22 -0700

    Added functions, must figure out seeding

--- Generator.js
+++ Generator.js
@@ -1,17 +1,17 @@
 const Calculator = require('./Calculator');
 class Generator extends Calculator {
 	/*
-	randomNoSeed() {
-		
+	randomNoSeed(min, max) {
+		return (Math.random() * (max - min + 1) + min);
 	}
-	randomSeed(){
-		
+	randomSeed(min, max, seed){
+		return (Math.random() * (max - min + 1) + min);
 	}
-	randomNumberList() {
-		
+	randomNumberList(max) {
+		return Array.from({length: max}, () => Math.floor(Math.random() * max));
 	}
-	chooseRandomItem() {
-		
+	chooseRandomItem(anAray) {
+		return anArray[Math.floor(Math.random() * anArray.length)];
 	}
 	selectSameValue() {
 		
