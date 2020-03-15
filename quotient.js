54484cd7a1eb3a42898117c74d62c19577269601
Tomas Pasiecznik <42984839+tomaspasie@users.noreply.github.com>
2020-03-15 17:07:14 -0400

    Added Exception For Numbers Divided By Zero

--- quotient.js
+++ quotient.js
@@ -1,6 +1,10 @@
 // Division
 function quotient(a,b){
-    return a/b;
+    if(b==0){
+        throw "Error: Denominator cannot be 0."
+    } else{
+        return a/b;
+    }
 }
 
 module.exports = quotient;
