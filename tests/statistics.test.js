906140cd583708aee2919fc80f1e113cf095c798
Tomas Pasiecznik <42984839+tomaspasie@users.noreply.github.com>
2020-03-14 20:39:55 -0400

    Update statistics.test.js

--- tests/statistics.test.js
+++ tests/statistics.test.js
@@ -1,6 +1,6 @@
 const calculator = require('../calculator');
 let calc = new calculator;
 
-test('The calculator returns the mean of the values.', () => {
-    expect(calc.Mean()).toBe();
-});
+//test('The calculator returns the mean of the values.', () => {
+//    expect(calc.Mean()).toBe();
+//});
