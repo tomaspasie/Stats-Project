1e2b23df2dc3d6d2cf77e4d3380d623c74957ac0
Tomas Pasiecznik <42984839+tomaspasie@users.noreply.github.com>
2020-03-16 00:00:41 -0400

    Update quotient.js

--- quotient.js
+++ quotient.js
@@ -1,7 +1,7 @@
 // Division
 function quotient(a,b){
     if(b==0){
-        throw "Error: Denominator cannot be 0."
+        throw "Error: Denominator cannot be 0.";
     } else{
         return a/b;
     }
