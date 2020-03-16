3abf7f1b5a50e1d4d4240e9d1c6b1f2637159026
Tomas Pasiecznik <42984839+tomaspasie@users.noreply.github.com>
2020-03-15 23:58:41 -0400

    Added All Statistics Tests

--- tests/statistics.test.js
+++ tests/statistics.test.js
@@ -1,91 +1,75 @@
-const calculator = require('./statistics');
+const calculator = require('../statistics');
 const generate = require('../Generator');
+var jStat = require('jStat');
 let calc = new calculator;
 let generator = new generate;
 
 // Statistics Functions
 
 test('Mean', () => {
-    expect(generator.randomSeed(0, 10, 'seed')).toBe(5.661807692527293); // Change Values to Numbers From Random Generator
+    expect(calc.Mean(generator.randomNumberList(10, 1, 10, 'test'))).toBe(jStat.mean(generator.randomNumberList(10, 1, 10, 'test')));
 });
 
-/*
-
 test('Median', () => {
-    expect().toBe();
+    expect(calc.Median([1,2,3,4,5,6])).toBe(jStat.median([1,2,3,4,5,6]));
 });
 
 test('Mode', () => {
-    expect().toBe();
+    expect(calc.Mode([1,2,3,3,3,4,5])).toBe("3");
 });
 
 test('Variance', () => {
-    expect().toBe();
+    expect(calc.Variance(generator.randomNumberList(10, 1, 10, 'test'))).toBe(jStat.variance(generator.randomNumberList(10, 1, 10, 'test')));
 });
 
 test('Standard Deviation', () => {
-    expect().toBe();
+    expect(calc.standardDev(generator.randomNumberList(10, 1, 10, 'test'))).toBe(jStat.stdev(generator.randomNumberList(10, 1, 10, 'test')));
 });
 
+
 test('Quartiles', () => {
-    expect().toBe();
+    expect(calc.Quartiles([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])).toBe("Lower Quartile (Q1): 4 | Middle Quartile (Q2): 8 | Upper Quartile (Q3): 12");
 });
 
 test('Skewness', () => {
-    expect().toBe();
+    expect(calc.Skewness(generator.randomNumberList(10, 1, 10, 'test'))).toBe(jStat.skewness(generator.randomNumberList(10, 1, 10, 'test')));
 });
 
 test('Sample Correlation', () => {
-    expect().toBe();
+    expect(calc.sampleCorrelation([15,37,59,78,91],[6,21,48,67,88])).toBe(1.2407801867935795);
 });
 
 test('Population Correlation', () => {
-    expect().toBe();
+    expect(calc.populationCorrelation([15,37,59,78,91],[6,21,48,67,88])).toBe(0.9926241494348637);
 });
 
 test('Z-Score', () => {
-    expect().toBe();
+    expect(calc.zScore(generator.randomNumberList(10, 1, 10, 'test'), generator.selectSameValue(generator.randomNumberList(10, 1, 10, 'test'),'seed'))).toBe(jStat.zscore(generator.selectSameValue(generator.randomNumberList(10, 1, 10, 'test'),'seed'),generator.randomNumberList(10, 1, 10, 'test')));
 });
 
 test('Mean Deviation', () => {
-    expect().toBe();
+    expect(calc.meanDeviation(generator.randomNumberList(10, 1, 10, 'test'))).toBe(jStat.meandev(generator.randomNumberList(10, 1, 10, 'test')));
 });
 
-*/
 
 // Population Sampling Functions
 
-/*
-test('Simple Random Sampling', () => {
-    expect().toBe();
-});
-
 test('Simple Systematic Sampling', () => {
-    expect().toBe();
+    expect(calc.systematicSampling([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)).toBe("2,4,6,8,10");
 });
 
 test('Sample Confidence Interval', () => {
-    expect().toBe();
-});
-
-test('Margin of Error', () => {
-    expect().toBe();
-});
-
-test('Population Confidence Interval', () => {
-    expect().toBe();
+    expect(calc.confidenceInterval([15,37,59,78,91,6,21,48,67,88])).toBe("32.9893169479889 ± 69.0106830520111");
 });
 
 test('Cochran’s Sample Size', () => {
-    expect().toBe();
+    expect(calc.cochransFormula([15,37,59,78,91,6,21,48,67,88],5)).toBe(3.5714285714285716);
 });
 
 test('Sample Size (Unknown Population Standard Deviation)', () => {
-    expect().toBe();
+    expect(calc.unknownPopStDev(.50)).toBe(1068);
 });
 
 test('Sample Size (Known Population Standard Deviation)', () => {
-    expect().toBe();
+    expect(calc.knownPopStDev(2.5)).toBe(97);
 });
-
-*/
